9946b98a7629915c94df2bd31abe1f57095b8526
U <nils.del@protonmail.com>
2024-01-27 08:54:08 +0100

    fix: avoid playing hero in custom viewers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="relative w-full h-screen flex">
         
         {/* Video */}
-        <video src={require('../../public/hero-video.mp4')} autoPlay muted loop className="absolute w-full h-full object-cover z-[1]" />
+        <video src={require('../../public/hero-video.mp4')} autoPlay muted loop playsInline className="absolute w-full h-full object-cover z-[1]" />
 
         {/* Title - Positioned Significantly Above Center */}
         <div className="absolute z-10 text-3xl font-medium tracking-widest uppercase text-white top-48 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
