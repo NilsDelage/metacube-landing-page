6c2bf31cd38f99330a2ca1fb4ecde385f969c9b7
U <nils.del@protonmail.com>
2024-02-04 17:53:03 +0100

    style: changed about icon for mobile

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,17 @@
 "use client";
 import React from "react";
 import { FloatingNav } from "../ui/navbar";
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+//q: whats the icon for documents ?
+//a: IconInfoCircle
+//q: whats the icon for documents apart from IconInfoCircle ?
+
+import {IconFileText} from "@tabler/icons-react";
 export function Navbar() {
   const navItems = [
     {
       name: "About",
       link: "https://metacube-1.gitbook.io/metacube/",
-      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <IconFileText className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
   ];
   return (
